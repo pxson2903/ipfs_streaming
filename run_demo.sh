d8b996903fc222d6404801fa4bde04886398ff1a
iOSdeveloper <dovuthanh2018@gmail.com>
2019-07-22 09:46:50 +0700

    updated demo page

--- run_demo.sh
+++ run_demo.sh
@@ -1,20 +1,32 @@
 # Hello World Program in Bash Shell
 
+echo "shutdown ipfs"
+
+
 echo "Load demo script"
 
 echo "check ip address from eth1 and upate route"
 
-ip=$(ip -f inet -o addr show eth0|cut -d\  -f 7 | cut -d/ -f 1)
-echo "$ip"
+ipTable=$(ip r | grep "default")
+gwTable=$(echo "$ipTable"| grep "eth1")
+#echo "$gwTable"
+gw=$(echo "$gwTable" |cut -d' ' -f3)
 
-#ip route add default via "$ip" dev eth1
+echo "$gw"
+
+
+#ip route add default via "$gw" dev eth1
 
 echo "start Alrogand and connect to testnet"
 
-#goal node start -d ~/node/testnetdata
+goal node start -d ~/node/testnetdata
+
+sleep 3
 
 echo "check Algorand status"
-#goal node status -d ~/node/testnetdata
+goal node status -d ~/node/testnetdata
+
+sleep 3
 
 echo "start web demo"
 
